6d3a6350984e0b2fb135cfcb6a95b10324e6a319
donggggas <55533895+donggggas@users.noreply.github.com>
2024-04-30 16:08:52 +0900

    [Diamond III] Title: 로봇, Time: 200 ms, Memory: 13744 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/17617. 로봇/로봇.cc
+++ 백준/Diamond/17617. 로봇/로봇.cc
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v(1000001),dp(1000001);
-int n,r,m,res;
+vector<int> v(1000001),dp(2000002);
+int n,r,m,res,temp;
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     cin >> n >> r >> m;
     for(int i = 0; i < n; i++) cin >> v[i];
     sort(v.begin(),v.begin()+n);
     v[n] = v[0]+m;
-    for(int i = 0; i < n; i++) dp[i] = v[i+1]-v[i]-2*r;
-    for(int i = 0; i < n; i++) res = max(dp[i],res);
+    for(int i = 0; i < n; i++) dp[i+n] = dp[i] = v[i+1]-v[i]-2*r;
+    for(int i = 0; i < 2*n-1; i++){
+        temp += dp[i];
+        if(temp < 0) temp = 0;
+        res = max(res,temp);
+    }
     cout << (res+1)/2;
 }
